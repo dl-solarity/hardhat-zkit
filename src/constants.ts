3bbfce3bf21c7f492022f977562e28702ceab805
Oleh Komendant <oleg02kom@gmail.com>
2024-08-02 13:22:59 +0300

    Update pragma reg exp

--- src/constants.ts
+++ src/constants.ts
@@ -16,13 +16,13 @@ export const MAX_PTAU_ID = 27;
 export const PTAU_FILE_REG_EXP = /^(?:.+-|)(\d{1,2}).ptau$/;
 
 export const INCLUDE_REG_EXP = /^\s*include\s+"([.\-/\w]+)";/gm;
-export const PRAGMA_VERSION_REG_EXP = /pragma +circom +(\d\.\d\.\d) *;/g;
+export const PRAGMA_VERSION_REG_EXP = /pragma\s+circom\s+(\d\.\d\.\d);/g;
 export const CIRCOM_FILE_REG_EXP = /\w+\.circom/;
 
 export const NODE_MODULES_REG_EXP = /^node_modules\//;
 export const URI_SCHEME_REG_EXP = /([a-zA-Z]+):\/\//;
 
 export const MAIN_COMPONENT_REG_EXP =
-  /component\s+main\s*(?:{\s*public\s+\[\s*(?:\w+,?\s*)+\]\s*}|)\s*=\s*([\w-]+)\s*\((?:\d+,?\s*|\s*)*\)\s*;\s*$/gm;
+  /component\s+main\s*(?:{\s*public\s+\[\s*(?:\w+,?\s*)+\]\s*}|)\s*=\s*([\w-]+)\s*\((?:\d+,?\s*|\s*)*\)\s*;/gm;
 
 export const CIRCUIT_ARTIFACTS_SUFFIX = "_artifacts.json";
