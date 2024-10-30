88552f1955188a9aa51b2a8c0b2d7079fa59152c
mllwchrry <mariia.zhvanko@gmail.com>
2024-10-30 18:15:34 +0200

    removed dockerized coverage

--- Dockerfile
+++ Dockerfile
@@ -2,13 +2,10 @@ ARG NODE_VERSION=20.0.0
 
 FROM node:${NODE_VERSION}-alpine
 
-ARG RUN_COMMAND=test-local
-ENV RUN_COMMAND=$RUN_COMMAND
-
 WORKDIR /hardhat-zkit
 
 COPY . .
 
 RUN npm install
 
-RUN npm run ${RUN_COMMAND}
+RUN npm run test-local
--- scripts/docker-coverage.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-echo "Running coverage on node 20.0.0"
-docker build --build-arg RUN_COMMAND=coverage-local -t hardhat-zkit-coverage-runner --progress=plain --no-cache .
-
-echo "Removing Docker image"
-docker rmi hardhat-zkit-coverage-runner --force
